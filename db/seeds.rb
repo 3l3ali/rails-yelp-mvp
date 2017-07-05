59046cab2c76e1d8bb24c822de2668ed17ba8690
Mohammad Alali <m.a.alsyouf@gmail.com>
2017-07-05 15:52:29 +0900

    done with all backend and front end functions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+5.times do
+  Restaurant.create(name: Faker::Company.name, address: Faker::Address.state, category: Restaurant::CATEGORIES.sample)
+end
